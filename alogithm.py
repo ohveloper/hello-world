95a08c1fb474fd012873920ce9bf5c7815c16337
CSE_오민교 <ohveloper@CSE-omingyoui-MacBookPro.local>
2021-06-27 23:35:49 +0900

    python study

--- alogithm.py
+++ alogithm.py
@@ -1,1 +1,8 @@
-print("alogrithm")
+def solution(participant, completion):
+    _participant = participant.sort()
+    _completion = completion.sort()
+    test = "helloworld"
+    return print(test[:5])
+    
+
+solution(["leo", "kiki", "eden"],	["eden", "kiki"])
